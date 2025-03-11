6c0f7c472a9fd623c0e025315b7f7b729e004894
EmmaH07 <harelemma07@gmail.com>
2025-03-11 15:10:17 +0200

    changed the current functions to use specific columns and also closed the cursors after use

--- db_connector.py
+++ db_connector.py
@@ -13,53 +13,44 @@ class DBConnection:
 
     def movie_exists(self, movie_name):
         my_cursor = self.__db.cursor()
-        my_cursor.execute('SELECT movie_name FROM movies')
-        movies = my_cursor.fetchall()
-        ret_ans = False
-        for movie in movies:
-            if movie_name == movie[0]:
-                ret_ans = True
-        return ret_ans
+        my_cursor.execute('SELECT movie_name FROM movies WHERE movie_name = %s', (movie_name,))
+        movie = my_cursor.fetchone()
+        my_cursor.close()
+        return movie is not None
 
     def fetch_movie(self, movie_name):
         my_cursor = self.__db.cursor()
-        my_cursor.execute('SELECT * FROM movies')
-        movies = my_cursor.fetchall()
-        ret_ans = ''
-        for movie in movies:
-            if movie_name == movie[0]:
-                ret_ans = movie[1]
-        return ret_ans
+        my_cursor.execute('SELECT * FROM movies WHERE movie_name = %s', (movie_name,))
+        movie = my_cursor.fetchone()
+        my_cursor.close()
+        return movie[1] if movie else None
 
     def username_exists(self, name):
         my_cursor = self.__db.cursor()
-        my_cursor.execute('SELECT username FROM users')
-        users = my_cursor.fetchall()
-        ret_ans = False
-        for user in users:
-            if name == user[0]:
-                ret_ans = True
-        return ret_ans
+        my_cursor.execute('SELECT username FROM users WHERE username = %s', (name,))
+        user = my_cursor.fetchone()
+        my_cursor.close()
+        return user is not None
 
     def user_exists(self, username, password_in_hash):
         my_cursor = self.__db.cursor()
-        my_cursor.execute('SELECT username FROM users')
-        users = my_cursor.fetchall()
-        ret_ans = False
-        for user in users:
-            if username == user[0]:
-                if password_in_hash == user[1]:
-                    ret_ans = True
-        return ret_ans
+        my_cursor.execute('SELECT user_password FROM users WHERE username = %s', (username,))
+        user = my_cursor.fetchone()
+        my_cursor.close()
+        return user is not None and user[0] == password_in_hash
 
     def add_user(self, username, password):
         cursor = self.__db.cursor()
-
-        sql = "INSERT INTO users (name, password) VALUES (%s, %s, %s)"
-        values = (username, password)
-
-        cursor.execute(sql, values)
-        self.__db.commit()
+        try:
+            sql = "INSERT INTO users (username, user_password) VALUES (%s, %s)"
+            cursor.execute(sql, (username, password))
+            self.__db.commit()
+            return True  # Success
+        except mysql.connector.IntegrityError:
+            print("Error: Username already exists.")
+            return False  # Failure
+        finally:
+            cursor.close()
 
 
 if __name__ == '__main__':
