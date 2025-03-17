415c61df062f17f35a798faec2685966e5746e08
EmmaH07 <harelemma07@gmail.com>
2025-03-17 12:57:57 +0200

    working sign up & login and added a home page.

--- gui_alpha.py
+++ gui_alpha.py
@@ -4,9 +4,15 @@ import re
 import socket
 import flashpoint_protocol
 import hashlib
+import os
 
 IP = '127.0.0.1'
 PORT = 3514
+HOME_X1 = 100
+HOME_X2 = 411
+HOME_X3 = 722
+HOME_X4 = 1033
+HOME_Y = 250
 
 
 def check_user_qualifications(username, password):
@@ -28,6 +34,95 @@ def slider():
         txt_label.after(400, slider)
 
 
+def get_paused_movies(username, password, client_socket):
+    msg = flashpoint_protocol.create_proto_msg('GM', flashpoint_protocol.create_proto_data(username, password))
+    client_socket.send(msg.encode())
+    ret_msg = flashpoint_protocol.get_proto_msg(client_socket)
+    ret_dict = {}
+    if flashpoint_protocol.get_func(ret_msg) == 'YM':
+        movie_str = flashpoint_protocol.get_data(ret_msg, 1)
+        frames_str = flashpoint_protocol.get_data(ret_msg, 2)
+        if movie_str and frames_str:
+            key_lst = movie_str.split('|')
+            val_lst = frames_str.split('|')
+            count = 0
+            max_len = len(key_lst)
+            if len(val_lst) < len(key_lst):
+                max_len = len(val_lst)
+            while count < max_len:
+                ret_dict[key_lst[count]] = val_lst[count]
+                count += 1
+    return ret_dict
+
+
+def display_movies(client_socket, movie_dict, counter=0):
+    global home_pg_frame
+    poster1, poster2, poster3, poster4 = '', '', '', ''
+    added = 0
+    movie_lst = [*movie_dict]
+    while counter < len(movie_lst) and added < 4:
+        poster_msg = flashpoint_protocol.create_proto_msg('GP',
+                                                          flashpoint_protocol.create_proto_data(movie_lst[counter]))
+        client_socket.send(poster_msg)
+        ret_msg = flashpoint_protocol.get_proto_msg(client_socket)
+        if flashpoint_protocol.get_func(ret_msg) == 'MP':
+            if added == 0:
+                poster1 = flashpoint_protocol.get_data(ret_msg)
+                if os.path.exists(poster1) and poster1.endswith('.png'):
+                    poster1 = PhotoImage(poster1)
+                else:
+                    poster1 = ''
+
+            if added == 1:
+                poster2 = flashpoint_protocol.get_data(ret_msg)
+                if os.path.exists(poster2) and poster2.endswith('.png'):
+                    poster2 = PhotoImage(poster2)
+                else:
+                    poster2 = ''
+
+            if added == 2:
+                poster3 = flashpoint_protocol.get_data(ret_msg)
+                if os.path.exists(poster3) and poster3.endswith('.png'):
+                    poster3 = PhotoImage(poster3)
+                else:
+                    poster3 = ''
+
+            if added == 3:
+                poster4 = flashpoint_protocol.get_data(ret_msg)
+                if os.path.exists(poster4) and poster4.endswith('.png'):
+                    poster4 = PhotoImage(poster4)
+                else:
+                    poster4 = ''
+
+        counter += 1
+        added += 1
+
+    if poster1 == '' and poster2 == '' and poster3 == '' and poster4 == '':
+        empty_home_pg()
+
+    else:
+        p_label1 = Label(home_pg_frame, image=poster1)
+        p_label1.place(x=HOME_X1, y=HOME_Y)
+        p_label2 = Label(home_pg_frame, image=poster2)
+        p_label2.place(x=HOME_X2, y=HOME_Y)
+        p_label3 = Label(home_pg_frame, image=poster3)
+        p_label3.place(x=HOME_X3, y=HOME_Y)
+        p_label4 = Label(home_pg_frame, image=poster4)
+        p_label4.place(x=HOME_X4, y=HOME_Y)
+
+
+def empty_home_pg():
+    global home_pg_frame, empty_poster
+    p_label1 = Label(home_pg_frame, image=empty_poster, background='#262626')
+    p_label1.place(x=HOME_X1, y=HOME_Y)
+    p_label2 = Label(home_pg_frame, image=empty_poster, background='#262626')
+    p_label2.place(x=HOME_X2, y=HOME_Y)
+    p_label3 = Label(home_pg_frame, image=empty_poster, background='#262626')
+    p_label3.place(x=HOME_X3, y=HOME_Y)
+    p_label4 = Label(home_pg_frame, image=empty_poster, background='#262626')
+    p_label4.place(x=HOME_X4, y=HOME_Y)
+
+
 def login_submit(client_socket):
     global login_frame
     username = login_username_box.get()
@@ -42,14 +137,19 @@ def login_submit(client_socket):
         login_msg = flashpoint_protocol.create_proto_msg('LI',
                                                          flashpoint_protocol.create_proto_data(username, password))
         client_socket.send(login_msg.encode())
+        print(login_msg)
         ret_msg = flashpoint_protocol.get_proto_msg(client_socket)
+        print(ret_msg)
         exists = flashpoint_protocol.get_data(ret_msg)
+        print(exists)
         if exists == 'False':
             err_txt = "User doesn't exist"
             err_txt_label = Label(login_frame, text=err_txt, font=('ariel narrow', 15, 'bold'), fg='white',
                                   bg='#262626')
             err_txt_label.place(y=700, x=575)
             err_txt_label.after(3000, err_txt_label.destroy)
+        else:
+            home_screen(client_socket, username, password)
 
 
 def signup_submit(client_socket):
@@ -66,6 +166,7 @@ def signup_submit(client_socket):
         signup_msg = flashpoint_protocol.create_proto_msg('SU',
                                                           flashpoint_protocol.create_proto_data(username, password))
         client_socket.send(signup_msg.encode())
+        print(signup_msg)
         ret_msg = flashpoint_protocol.get_proto_msg(client_socket)
         print(ret_msg)
         exists = flashpoint_protocol.get_data(ret_msg)
@@ -83,26 +184,43 @@ def signup_submit(client_socket):
             ok_txt_label.after(3000, ok_txt_label.destroy)
 
 
-def login_screen(my_socket):
+def login_screen(client_socket):
     global current_frame
     if current_frame:
         current_frame.pack_forget()  # Hide current frame
     current_frame = login_frame
     login_frame.pack()  # Show login frame
-    create_login_frame(my_socket)  # Ensure UI elements are initialized properly
+    create_login_frame(client_socket)  # Ensure UI elements are initialized properly
 
 
-
-def signup_screen(my_socket):
+def signup_screen(client_socket):
     global current_frame
     if current_frame:
         current_frame.pack_forget()  # Hide current frame
     current_frame = signup_frame
     signup_frame.pack()  # Show signup frame
-    create_signup_frame(my_socket)  # Ensure UI elements are initialized properly
+    create_signup_frame(client_socket)  # Ensure UI elements are initialized properly
 
 
-def create_login_frame(my_socket):
+def library_screen(client_socket, username, password):
+    global current_frame
+    if current_frame:
+        current_frame.pack_forget()
+    current_frame = lib_frame
+    lib_frame.pack()
+    create_lib_pg(client_socket, username, password)
+
+
+def home_screen(client_socket, username, password):
+    global current_frame
+    if current_frame:
+        current_frame.pack_forget()
+    current_frame = home_pg_frame
+    home_pg_frame.pack()
+    create_home_pg(client_socket, username, password)
+
+
+def create_login_frame(client_socket):
     global login_username_box, login_password_box, login_frame, start_bg, user_dict
     bg_label = Label(login_frame, image=start_bg)
     bg_label.place(x=0, y=0)
@@ -233,6 +351,75 @@ def create_signup_frame(my_socket):
     signup_frame.pack()
 
 
+def create_home_pg(client_socket, username, password):
+    global home_pg_frame, library_bg
+    bg_label = Label(home_pg_frame, image=library_bg)
+    bg_label.place(x=0, y=0)
+    movie_dict = get_paused_movies(username, password, client_socket)
+    display_movies(client_socket, movie_dict)
+    change_2lib = Button(home_pg_frame, text='Library', command=lambda: library_screen(my_socket, username, password),
+                         activebackground="#7d0101",
+                         activeforeground="#fcba03",
+                         anchor="center",
+                         bd=3,
+                         bg="#c00000",
+                         cursor="hand2",
+                         disabledforeground="#fcba03",
+                         fg="#fcba03",
+                         font=("Arial Narrow", 18),
+                         width=11)
+    change_2lib.place(x=1200, y=10)
+
+    change_2home = Button(home_pg_frame, text='Home', command=lambda: home_screen(client_socket, username, password),
+                          activebackground="#7d0101",
+                          activeforeground="#fcba03",
+                          anchor="center",
+                          bd=3,
+                          bg="#7d0101",
+                          cursor="hand2",
+                          disabledforeground="#fcba03",
+                          fg="#fcba03",
+                          font=("Arial Narrow", 18),
+                          width=11)
+    change_2home.place(x=1060, y=10)
+    change_2home.config(state=tk.DISABLED)
+    home_pg_frame.pack()
+
+
+def create_lib_pg(client_socket, username, password):
+    global lib_frame, library_bg
+    bg_label = Label(lib_frame, image=library_bg)
+    bg_label.place(x=0, y=0)
+    movie_dict = get_paused_movies(username, password, client_socket)
+    change_2lib = Button(lib_frame, text='Library', command=lambda: library_screen(my_socket, username, password),
+                         activebackground="#7d0101",
+                         activeforeground="#fcba03",
+                         anchor="center",
+                         bd=3,
+                         bg="#7d0101",
+                         cursor="hand2",
+                         disabledforeground="#fcba03",
+                         fg="#fcba03",
+                         font=("Arial Narrow", 18),
+                         width=11)
+    change_2lib.place(x=1200, y=10)
+    change_2lib.config(state=DISABLED)
+
+    change_2home = Button(lib_frame, text='Home', command=lambda: home_screen(client_socket, username, password),
+                          activebackground="#7d0101",
+                          activeforeground="#fcba03",
+                          anchor="center",
+                          bd=3,
+                          bg="#c00000",
+                          cursor="hand2",
+                          disabledforeground="#fcba03",
+                          fg="#fcba03",
+                          font=("Arial Narrow", 18),
+                          width=11)
+    change_2home.place(x=1060, y=10)
+    lib_frame.pack()
+
+
 win = Tk()  # creates a window instance
 win.geometry('1365x768')  # set window size
 win.title('Flashpoint.io')  # set title
@@ -241,6 +428,8 @@ win.iconphoto(True, icon)  # set window icon
 
 # set images
 start_bg = PhotoImage(file='start_bg.png')
+library_bg = PhotoImage(file='library_bg.png')
+empty_poster = PhotoImage(file='empty_poster.png')
 
 # set background
 clean_bg = PhotoImage(file='clean_bg.png')
@@ -250,6 +439,8 @@ label1.place(x=0, y=0)
 # set frames
 login_frame = Frame(win, bg='black', height=768, width=1365)
 signup_frame = Frame(win, bg='black', height=768, width=1365)
+home_pg_frame = Frame(win, bg='black', height=768, width=1365)
+lib_frame = Frame(win, bg='black', height=768, width=1365)
 
 # setting the login frame as default
 current_frame = login_frame
