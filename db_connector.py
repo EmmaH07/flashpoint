153226c8ff1402fb2d0b4dbfeca4aa483cbe03fa
EmmaH07 <harelemma07@gmail.com>
2025-03-17 12:55:59 +0200

    added a get movie list function

--- db_connector.py
+++ db_connector.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import pickle
 
 
 class DBConnection:
@@ -52,8 +53,47 @@ class DBConnection:
         finally:
             cursor.close()
 
+    def get_movie_lst(self, user_id):
+        cursor = self.__db.cursor()
+        cursor.execute('SELECT movie_name, last_frame FROM seen_movies WHERE userID = %s',
+                       (user_id,))
+        user = cursor.fetchall()
+        cursor.close()
+        return user
+
+    def update_movie_lst(self, username, password, movie_to_add, last_frame):
+        cursor = self.__db.cursor()
+        cursor.execute('SELECT (movies, frames) FROM users WHERE username = %s AND user_password = %s',
+                       (username, password))
+        user = cursor.fetchone()
+        movie_str = ''
+        frames_str = ''
+        if user[0] is not None and user[1] is not None:
+            movie_str = user[0]
+            frames_str = user[1]
+
+        if movie_to_add != '' and last_frame != '' and movie_str != '':
+            movie_str += '|' + movie_to_add
+            frames_str += '|' + last_frame
+
+        elif movie_str == '' and movie_to_add != '' and last_frame != '':
+            movie_str += movie_to_add
+            frames_str += last_frame
+
+        try:
+            sql = "INSERT INTO users (movies, frames) VALUES (%s, %s) WHERE username = %s AND user_password = %s'"
+            cursor.execute(sql, (movie_str, frames_str, username, password))
+            self.__db.commit()
+            return True  # Success
+        except mysql.connector.IntegrityError:
+            print("Error: Username already exists.")
+            return False  # Failure
+        finally:
+            cursor.close()
+
 
 if __name__ == '__main__':
     db = DBConnection()
     print(db.movie_exists('The Flash'))
     print(db.fetch_movie('The Flash'))
+    print('movie_lst:' + str(db.get_movie_lst(1)))
