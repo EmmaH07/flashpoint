7b95bacb4b7c6e5c9a2d307d80644d5d06566ef5
EmmaH07 <harelemma07@gmail.com>
2025-04-03 14:39:01 +0300

    added creation and receiving of a bytes-based message

--- flashpoint_protocol.py
+++ flashpoint_protocol.py
@@ -11,22 +11,60 @@ def get_func(proto_msg):
     return func_str
 
 
+def get_byte_func(byte_msg):
+    ret_chunk = b''
+    is_chunk = False
+    is_func = True
+    for c in byte_msg:
+        c = bytes([c])
+        if is_chunk:
+            if c == b'|':
+                is_func = False
+            if is_func:
+                ret_chunk += c
+        if c == b'@':
+            is_chunk = True
+    return ret_chunk.decode()
+
+
 def get_data(proto_msg, half_num=1):
     if half_num < 1 or half_num > 2:
         half_num = 1
     return proto_msg.split('|')[1].split('^')[half_num - 1]
 
 
-def get_chunk(chunk_msg):
+def get_bytes_second_data_half(byte_msg):
     ret_chunk = b''
     is_chunk = False
-    for c in chunk_msg:
+    for c in byte_msg:
         c = bytes([c])
         if is_chunk:
             ret_chunk += c
         if c == b'^':
             is_chunk = True
     return ret_chunk
+
+
+def get_bytes_data(byte_msg, data_half=1):
+    ret_chunk1 = b''
+    ret_chunk2 = b''
+    is_chunk = False
+    first_half = True
+    for c in byte_msg:
+        c = bytes([c])
+        if is_chunk:
+            if c == b'^':
+                first_half = False
+            if first_half:
+                ret_chunk1 += c
+            else:
+                ret_chunk2 += c
+        if c == b'|':
+            is_chunk = True
+    ret_data = ret_chunk1
+    if data_half == 2:
+        ret_data = ret_chunk2
+    return ret_data
 
 
 def get_proto_msg(client_socket):
@@ -43,7 +81,7 @@ def get_proto_msg(client_socket):
     return full_msg
 
 
-def get_chunk_msg(client_socket):
+def get_byte_msg(client_socket):
     curr_char = client_socket.recv(1)
     msg_len = b''
     while curr_char != b'@':
@@ -86,7 +124,12 @@ def create_chunk_data(chunk_num, chunk):
     return msg
 
 
-def create_chunk_msg(data):
-    msg_bytes = b'MC|' + data
+def create_byte_data(data1=b'', data2=b''):
+    data = data1 + b'^' + data2
+    return data
+
+
+def create_byte_msg(func, data):
+    msg_bytes = func.encode() + b'|' + data
     msg = str(len(msg_bytes)).encode() + b'@' + msg_bytes
     return msg
