3565b1242bf45e06c16e6cfcd922f41ef21575e9
EmmaH07 <harelemma07@gmail.com>
2025-05-09 01:05:41 +0300

    final media server complete with logging, error handling and encryption

--- media_server.py
+++ media_server.py
@@ -1,4 +1,3 @@
-import base64
 import json
 import os
 import random
@@ -11,9 +10,12 @@ import select
 
 import flashpoint_protocol
 from adv_db import AdvDB
+from rsa import RsaEncryption
+from aes import AesEncryption
 
 IP = '0.0.0.0'
 ADMIN_IP = '127.0.0.1'
+MY_IP = '127.0.0.1'
 PORT = 1939
 ADMIN_PORT = 3600
 QUEUE_SIZE = 10
@@ -62,14 +64,16 @@ def get_video_duration(movie_path, chunk_duration=10):
     return int(movie_len)
 
 
-def file_break(movie_path, client_socket, admin_socket, port, frame=0):
+def file_break(movie_path, client_socket, client_aes, admin_socket, admin_aes, port, frame=0):
     """
     breaking the movie into 10 seconds movie chunks before sending to client
 
     :param movie_path: the movie file path
     :type movie_path: str
     :param client_socket: the client's socket
+    :param client_aes:
     :param admin_socket: the admin's socket
+    :param admin_aes:
     :param port: the media server's port
     :type port: int
     :param frame: the last frame the client stopped at
@@ -82,7 +86,7 @@ def file_break(movie_path, client_socket, admin_socket, port, frame=0):
     # sending a 'movie length' message to the client
     movie_len = get_video_duration(movie_path)
     msg_data = flashpoint_protocol.create_proto_data(str(movie_len - frame).encode())
-    msg = flashpoint_protocol.create_proto_msg('ML', msg_data)
+    msg = flashpoint_protocol.create_aes_msg('ML', msg_data, client_aes)
     client_socket.send(msg)
     logging.debug('Sent to client: ML')
 
@@ -113,8 +117,9 @@ def file_break(movie_path, client_socket, admin_socket, port, frame=0):
             rlist, _, _ = select.select([client_socket], [], [], 0.8)  # Use select with short timeout
             if rlist:
                 try:
-                    stop_msg = flashpoint_protocol.get_proto_msg(client_socket)
+                    stop_msg = flashpoint_protocol.get_aes_msg(client_socket, client_aes)
                     logging.debug('Got message from client: ' + flashpoint_protocol.get_func(stop_msg))
+
                     if flashpoint_protocol.get_func(stop_msg) == 'PM':
                         # updating the var in order to stop sending chunks to the client
                         send = False
@@ -128,7 +133,7 @@ def file_break(movie_path, client_socket, admin_socket, port, frame=0):
 
                         # asking admin server to update the database
                         data = flashpoint_protocol.create_proto_data(username, password, movie, frame)
-                        msg = flashpoint_protocol.create_proto_msg('UD', data)
+                        msg = flashpoint_protocol.create_aes_msg('UD', data, admin_aes)
 
                         try:
                             if admin_socket:
@@ -137,22 +142,25 @@ def file_break(movie_path, client_socket, admin_socket, port, frame=0):
                                 logging.debug('Sent message to admin: UD')
 
                                 # sending a disconnect message to client
-                                client_socket.send(flashpoint_protocol.create_proto_msg(
-                                    'DS', flashpoint_protocol.create_proto_data()))
+                                msg = flashpoint_protocol.create_aes_msg('DS',
+                                                                         flashpoint_protocol.create_proto_data(),
+                                                                         client_aes)
+                                client_socket.send(msg)
                                 logging.debug('Sent message to client: DS')
 
                                 # informing the admin server that the client disconnected
-                                admin_socket.send(flashpoint_protocol.create_proto_msg(
-                                    'DC', flashpoint_protocol.create_proto_data(ADMIN_IP.encode(),
-                                                                                str(port).encode())))
+                                msg = flashpoint_protocol.create_aes_msg('DC',
+                                                                         flashpoint_protocol.create_proto_data(
+                                                                             ADMIN_IP.encode(), str(port).encode()),
+                                                                         admin_aes)
+                                admin_socket.send(msg)
                                 logging.debug('Sent message to admin: DC')
 
                                 # updating the var to inform that the stop message was sent
                                 stop = True
 
                         except (BrokenPipeError, ConnectionResetError) as e:
-                            logging.error(f"Error sending stop message to admin: {e}")
-                            print(f"Error sending stop message to admin: {e}")
+                            logging.error("Error sending stop message to admin: " + str(e))
 
                     # checking if I got an error message
                     elif flashpoint_protocol.get_func(stop_msg) == 'ER':
@@ -160,8 +168,7 @@ def file_break(movie_path, client_socket, admin_socket, port, frame=0):
                         break
 
                 except Exception as e:
-                    logging.error(f"Error while handling stop message: {e}")
-                    print(f"Error while handling stop message: {e}")
+                    logging.error("Error while handling stop message: " + str(e))
                     break
 
             # If the socket is still open, send the chunk
@@ -170,11 +177,10 @@ def file_break(movie_path, client_socket, admin_socket, port, frame=0):
                 try:
                     with open(full_path, "rb") as f:
                         data = f.read()
-                        encoded = base64.b64encode(data)
-                        msg = flashpoint_protocol.create_proto_msg(
-                            'MC',
-                            flashpoint_protocol.create_proto_data(str(i).encode(), encoded)
-                        )
+                        msg = flashpoint_protocol.create_aes_msg('MC',
+                                                                 flashpoint_protocol.create_proto_data(str(i).encode(),
+                                                                                                       data),
+                                                                 client_aes)
                         client_socket.send(msg)
                         logging.debug('Sent to client: MC')
 
@@ -188,7 +194,7 @@ def file_break(movie_path, client_socket, admin_socket, port, frame=0):
 
     # if the server sent all the chunks and the client didn't stop in the middle, wait for disconnect from client
     if not stop:
-        stop_msg = flashpoint_protocol.get_proto_msg(client_socket)
+        stop_msg = flashpoint_protocol.get_aes_msg(client_socket, client_aes)
 
         if flashpoint_protocol.get_func(stop_msg) == 'PM':
             send = False
@@ -200,32 +206,37 @@ def file_break(movie_path, client_socket, admin_socket, port, frame=0):
             if int(frame.decode()) >= movie_len - 1:
                 frame = '-1'.encode()
             data = flashpoint_protocol.create_proto_data(username, password, movie, frame)
-            msg = flashpoint_protocol.create_proto_msg('UD', data)
+            msg = flashpoint_protocol.create_aes_msg('UD', data, admin_aes)
             try:
                 if admin_socket:
                     # sending UD message to admin server
                     admin_socket.send(msg)
 
                     # sending a disconnect message to client
-                    client_socket.send(flashpoint_protocol.create_proto_msg(
-                        'DS', flashpoint_protocol.create_proto_data()))
+                    msg = flashpoint_protocol.create_aes_msg('DS',
+                                                             flashpoint_protocol.create_proto_data(), client_aes)
+                    client_socket.send(msg)
+                    logging.debug('Sent message to client: DS')
 
                     # informing the admin server that the client disconnected
-                    admin_socket.send(flashpoint_protocol.create_proto_msg(
-                        'DC', flashpoint_protocol.create_proto_data(ADMIN_IP.encode(),
-                                                                    str(port).encode())))
+                    msg = flashpoint_protocol.create_aes_msg('DC', flashpoint_protocol.create_proto_data
+                    (ADMIN_IP.encode(), str(port).encode()), admin_aes)
+                    admin_socket.send(msg)
+                    logging.debug('Sent message to admin: DC')
 
                     # updating the var to inform that the stop message was sent
                     stop = True
 
             except (BrokenPipeError, ConnectionResetError) as e:
-                print(f"Error sending stop message to admin: {e}")
+                print("Error sending stop message to admin: " + str(e))
 
 
-def handle_thread(admin_sock, client_socket, client_address, my_port, db):
+def handle_thread(admin_sock, admin_aes, client_socket, client_address, my_port, db):
     """
 
     :param admin_sock: the admin server's socket
+    :param admin_aes:
+    :type admin_aes:
     :param client_socket: the client's socket
     :param client_address: the client's address
     :param my_port: the media server port
@@ -235,12 +246,28 @@ def handle_thread(admin_sock, client_socket, client_address, my_port, db):
     :return:
     """
 
+    # setting RSA object and public key
+    rsa_obj = RsaEncryption()
+    pub_key = rsa_obj.get_pub_key()
+
+    # sending first message
+    first_msg = flashpoint_protocol.create_proto_msg('PK', flashpoint_protocol.create_proto_data(pub_key))
+    client_socket.send(first_msg)
+
+    # waiting for 'AES Key' message and making sure the command in it is the correct one for the start
+    key_msg = flashpoint_protocol.get_rsa_msg(client_socket, rsa_obj)
+    while flashpoint_protocol.get_func(key_msg) != 'AK':
+        key_msg = flashpoint_protocol.get_rsa_msg(client_socket, rsa_obj)
+
+    key = flashpoint_protocol.get_data(key_msg)
+    aes_obj = AesEncryption(key)
+
     # checking that the server got the correct first func
-    first_msg = flashpoint_protocol.get_proto_msg(client_socket)
+    first_msg = flashpoint_protocol.get_aes_msg(client_socket, aes_obj)
     logging.debug('Got a message from client: ' + flashpoint_protocol.get_func(first_msg))
     while flashpoint_protocol.get_func(first_msg) != FIRST_FUNC:
+        first_msg = flashpoint_protocol.get_aes_msg(client_socket, aes_obj)
         logging.debug('Got a message from client: ' + flashpoint_protocol.get_func(first_msg))
-        first_msg = flashpoint_protocol.get_proto_msg(client_socket)
 
     m_name = flashpoint_protocol.get_data(first_msg).decode()
     frame = flashpoint_protocol.get_data(first_msg, 2)
@@ -258,7 +285,7 @@ def handle_thread(admin_sock, client_socket, client_address, my_port, db):
         client_socket.send('ERROR'.encode())
 
     else:
-        file_break(m_fpath, client_socket, admin_sock, my_port, frame)
+        file_break(m_fpath, client_socket, aes_obj, admin_sock, admin_aes, my_port, frame)
 
 
 def connect2admin():
@@ -270,7 +297,6 @@ def connect2admin():
         main_server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         main_server_sock.connect((ADMIN_IP, ADMIN_PORT))
         logging.debug("Connected to admin server.")
-        print("Connected to admin server.")
 
         return main_server_sock
 
@@ -281,11 +307,41 @@ def connect2admin():
         return None
 
 
+def start_encryption(client_socket):
+    # waiting for RSA public key
+    key_msg = flashpoint_protocol.get_proto_msg(client_socket)
+    print(key_msg)
+
+    # making sure client got the correct message
+    func = flashpoint_protocol.get_func(key_msg)
+    while func != 'PK':
+        key_msg = flashpoint_protocol.get_proto_msg(client_socket)
+        func = flashpoint_protocol.get_func(key_msg)
+
+    # sending encrypted message that contains AES key
+    rsa_obj = RsaEncryption()
+    print(flashpoint_protocol.get_data(key_msg))
+    pub_key = flashpoint_protocol.get_data(key_msg)
+    aes_obj = AesEncryption()
+    enc_key = aes_obj.get_key()
+
+    # using RSA encryption to send AES key
+    key_msg = flashpoint_protocol.create_rsa_msg('AK', flashpoint_protocol.create_proto_data(enc_key), rsa_obj,
+                                                 pub_key)
+    client_socket.send(key_msg)
+    print(key_msg)
+
+    return aes_obj
+
+
 def main():
     # connecting to the admin server
     admin_sock = connect2admin()
     if not admin_sock:
         return
+
+    # establishing encryption with admin
+    admin_aes = start_encryption(admin_sock)
 
     # setting a port
     port = random.randint(1024, 65535)
@@ -294,8 +350,8 @@ def main():
     db = AdvDB(True, 'movie', MOVIE_DICT)
 
     # sending server details to the database
-    msg = flashpoint_protocol.create_proto_msg('SD', flashpoint_protocol.create_proto_data(ADMIN_IP.encode(),
-                                                                                           str(port).encode()))
+    msg = flashpoint_protocol.create_aes_msg('SD', flashpoint_protocol.create_proto_data(
+        MY_IP.encode(), str(port).encode()), admin_aes)
     admin_sock.send(msg)
 
     # connecting to a client
@@ -307,7 +363,7 @@ def main():
             client_socket, client_address = server_socket.accept()
             print('connected')
             thread = Thread(target=handle_thread,
-                            args=(admin_sock, client_socket, client_address, port, db))
+                            args=(admin_sock, admin_aes, client_socket, client_address, port, db))
             thread.start()
 
     except socket.error as err:
