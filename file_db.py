f843badc213d0211369113c329c23aad1c01762f
EmmaH07 <harelemma07@gmail.com>
2025-05-28 12:27:50 +0300

    final FileDb class with full annotations and exceptions

--- file_db.py
+++ file_db.py
@@ -11,13 +11,13 @@ class FileDB(DictDB):
         super().__init__(dic)
         if '.' in file_name:
             file_name = file_name.split('.')[0]
-        self.__file_name__ = file_name + '.pkl'
+        self.__file_name = file_name + '.pkl'
         if not os.path.exists(file_name):
             self.file_dump()
 
     def file_dump(self):
         try:
-            with open(self.__file_name__, 'wb') as f:
+            with open(self.__file_name, 'wb') as f:
                 pickle.dump(self.__dic__, f)
 
         except Exception as err:
@@ -25,7 +25,7 @@ class FileDB(DictDB):
 
     def file_load(self):
         try:
-            with open(self.__file_name__, 'rb') as file:
+            with open(self.__file_name, 'rb') as file:
                 self.__dic__ = pickle.load(file)
 
         except Exception as err:
@@ -39,10 +39,15 @@ class FileDB(DictDB):
         :param new_val: the new value
         :return: True if it worked, False if it didn't.
         """
-        self.file_load()
-        b = super().set_val(key, new_val)
-        self.file_dump()
-        return b
+        try:
+            self.file_load()
+            result = super().set_val(key, new_val)
+            self.file_dump()
+            return result
+
+        except Exception as e:
+            print(f'Error setting key {key}: {e}')
+            return False
 
     def delete_data(self, key):
         """
@@ -50,10 +55,15 @@ class FileDB(DictDB):
         :param key: the wanted key
         :return: the value of said key. None if the key doesn't exist.
         """
-        self.file_load()
-        val = super().delete_data(key)
-        self.file_dump()
-        return val
+        try:
+            self.file_load()
+            val = super().delete_data(key)
+            self.file_dump()
+            return val
+
+        except Exception as e:
+            print(f'Error deleting key {key}: {e}')
+            return None
 
     def get_val(self, key):
         """
@@ -61,19 +71,32 @@ class FileDB(DictDB):
         :param key: the wanted key
         :return: the value of said key. None if the key doesn't exist.
         """
-        self.file_load()
-        val = super().get_val(key)
-        self.file_dump()
-        return val
+        try:
+            self.file_load()
+            val = super().get_val(key)
+            self.file_dump()
+            return val
+
+        except Exception as e:
+            print(f'Error getting value for key {key}: {e}')
+            return None
 
     def get_dict(self):
-        self.file_load()
-        var = super().get_dict()
-        self.file_dump()
-        return var
+        """
+        retrieves the dictionary from file
+        :return: the entire dictionary. None if an error accrued
+        """
+        try:
+            self.file_load()
+            val = super().get_dict()
+            self.file_dump()
+            return val
+
+        except Exception as e:
+            print(f'Error retrieving dictionary: {e}')
+            return None
 
 
 if __name__ == "__main__":
-    f_obj = FileDB()
+    f_obj = FileDB('test')
     f_obj.set_val('hi', 'shalom')
-
